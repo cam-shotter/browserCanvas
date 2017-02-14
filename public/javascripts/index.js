53e2035e364c4356c7d271e66e9620d5ba90b5a7
cam-shotter <cameronshotter@gmail.com>
2017-02-14 14:31:55 +1300

    initial commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,74 @@
+var canvas, ctx, flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+
+var brushColour = "black",
+    brushSize = 2;
+
+function init() {
+  canvas = document.getElementById('drawingArea');
+  ctx = canvas.getContext("2d");
+  w = canvas.width;
+  h = canvas.height;
+
+  canvas.addEventListener("mousemove", function (e) {
+      findxy('move', e)
+  }, false);
+  canvas.addEventListener("mousedown", function (e) {
+      findxy('down', e)
+  }, false);
+  canvas.addEventListener("mouseup", function (e) {
+      findxy('up', e)
+  }, false);
+  canvas.addEventListener("mouseout", function (e) {
+      findxy('out', e)
+  }, false);
+}
+
+function color(obj) {
+
+}
+
+function draw() {
+  ctx.beginPath();
+  ctx.moveTo(prevX, prevY);
+  ctx.lineTo(currX, currY);
+  ctx.strokeStyle = brushColour;
+  ctx.lineWidth = brushSize;
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function findxy(res, e) {
+  if (res == 'down') {
+    prevX = currX;
+    prevY = currY;
+    currX = e.clientX - canvas.offsetLeft;
+    currY = e.clientY - canvas.offsetTop;
+
+    flag = true;
+    dot_flag = true;
+    if (dot_flag) {
+        ctx.beginPath();
+        ctx.fillStyle = brushColour;
+        ctx.fillRect(currX, currY, 2, 2);
+        ctx.closePath();
+        dot_flag = false;
+    }
+  }
+  if (res == 'up' || res == "out") {
+    flag = false;
+  }
+  if (res == 'move') {
+    if (flag) {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+        draw();
+    }
+  }
+}
