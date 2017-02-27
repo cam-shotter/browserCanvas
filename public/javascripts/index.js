39609ef0c2e2fd52118a17c0d03b0da96b4555ca
cam-shotter <cameronshotter@gmail.com>
2017-02-27 17:42:25 +1300

    added pixel brush

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -48,25 +48,35 @@ function draw() {
     ctx.moveTo(prevX, prevY);
     ctx.lineTo(currX, currY);
     ctx.lineJoin = ctx.lineCap = "round";
+    ctx.shadowColor = ctx.shadowBlur = null;
     if(brushType == "Pencil") {
       ctx.strokeStyle = x;
       ctx.lineWidth = y;
+      ctx.stroke();
     } else if (brushType == "Spray Paint") {
       ctx.strokeStyle = ctx.shadowColor = x;
       ctx.lineWidth = ctx.shadowBlur = y;
+      ctx.stroke();
     } else if (brushType == "Highlighter") {
-
+      ctx.stroke();
     } else if (brushType == "Pen") {
       ctx.strokeStyle = x;
       ctx.lineWidth = getRandomInt(y-1, y+1);
+      ctx.stroke();
     } else if (brushType == "Pixels") {
-
+      for (var i = -10; i < 10; i+= 4) {
+        for (var j = -10; j < 10; j+= 4) {
+          if (Math.random() > 0.9) {
+            ctx.fillStyle = x;
+            ctx.fillRect(currX+i, currY+j, 4, 4);
+          }
+        }
+      }
     } else if (brushType == "Blur") {
 
     } else if (brushType == "Eraser") {
 
     }
-    ctx.stroke();
     ctx.closePath();
 }
 
