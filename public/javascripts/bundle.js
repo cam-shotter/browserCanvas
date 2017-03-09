588218830c4ac97ad050bef746eac00597683b02
cam-shotter <cameronshotter@gmail.com>
2017-03-09 22:27:48 +1300

    removed duplicate initalising function

---
+++ public/javascripts/bundle.js
@@ -0,0 +1,1 @@
+console.error("Error: Cannot find module '/Users/cameronshotter/Documents/Projects/JavaScript/browserBoard/public/javascripts/main.js' from '/Users/cameronshotter/Documents/Projects/JavaScript/browserBoard'");
---
+++ public/javascripts/main.js
@@ -0,0 +1,106 @@
+var brushStyles = require("./brushStyles.js");
+
+var canvas,
+    ctx,
+    isDrawing = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0;
+
+var x = "black",
+    y = 2;
+
+var brushType = "Pencil";
+
+var savedDrawing
+
+function init() {
+  canvas = document.getElementById('drawingArea');
+
+  console.log("brushstyles",brushStyles);
+
+  if (canvas.getContext) {
+          ctx = canvas.getContext("2d");
+
+          window.addEventListener('resize', resizeCanvas, true);
+          window.addEventListener('orientationchange', resizeCanvas, true);
+          resizeCanvas();
+        }
+
+  canvas.onmousedown = function(e) {
+    getMouseCoordinates(e)
+    isDrawing = true;
+  }
+  canvas.onmouseup = function(e) {
+    isDrawing = false;
+  }
+  canvas.onmousemove = function(e) {
+    if (isDrawing) {
+        getMouseCoordinates(e)
+        brushStyles.draw(ctx);
+    }
+  }
+}
+
+function resizeCanvas() {
+  save()
+  w = canvas.width = window.innerWidth;
+  h = canvas.height = window.innerHeight - 77;
+  load()
+}
+
+function getMouseCoordinates(e) {
+  prevX = currX;
+  prevY = currY;
+  currX = e.clientX - canvas.offsetLeft;
+  currY = e.clientY - canvas.offsetTop;
+}
+
+function selectBrushColor() {
+  x = document.getElementById("colorWheel").value
+}
+
+function selectBrushSize(obj) {
+  y = parseInt(obj.innerHTML);
+}
+
+function selectBrushType(obj) {
+  brushType = obj.innerText;
+}
+
+function changeDisplay(obj) {
+  if (obj.style.display !== "flex") {
+    obj.style.display = "flex";
+  } else {
+    obj.style.display = "";
+  }
+}
+
+function changeColorDisplay() {
+  var dropDown = document.getElementById("colorWheel")
+  changeDisplay(dropDown)
+}
+
+function changeSizeDisplay() {
+  var dropDown = document.getElementById("brushSize")
+  changeDisplay(dropDown)
+}
+
+function changeTypeDisplay() {
+  var dropDown = document.getElementById("brushType")
+  changeDisplay(dropDown)
+}
+
+function save() {
+  console.log("Canvas saved!")
+  var savedState = canvas.toDataURL()
+}
+
+function load() {
+  console.log("Canvas loaded!");
+}
+
+function clear() {
+  console.log("Canvas cleared!")
+}
